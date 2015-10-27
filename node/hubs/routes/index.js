3ae24c45c0c21fa48265c3f018d89436dda75b57
Andrey Kartashov <porter@porter.st>
2015-10-26 22:32:54 -0400

    UCSC hub RNA/dUTP tests

--- node/hubs/routes/index.js
+++ node/hubs/routes/index.js
@@ -9,7 +9,7 @@ var router = express.Router();
 //    res.render('index', { title: 'Express',requesthub: req.params['requesthub'] });
 //});
 
-var pool  = mysql.createPool({
+var pool = mysql.createPool({
     host: config.db.host,
     user: config.db.user,
     password: config.db.password,
@@ -17,30 +17,30 @@ var pool  = mysql.createPool({
     port: config.db.port
 });
 
-router.get('/:shadow/:file', function(req, res, next) {
+router.get('/:shadow/:file', function (req, res, next) {
     res.set('Content-Type', 'text/plain');
 
     console.log(req.params);
 
-    switch(req.params['file']){
+    switch (req.params['file']) {
         case "hub.txt":
-            pool.getConnection(function(err, connection) {
+            pool.getConnection(function (err, connection) {
                 connection.query(
-                    'SELECT name,COALESCE(description,"SciDAP") as description from egroup where shadow=?',[req.params['shadow']], function (err, rows, fields) {
+                    'SELECT name,COALESCE(description,"SciDAP") as description from egroup where shadow=?', [req.params['shadow']], function (err, rows, fields) {
                         if (err == null && rows.length > 0)
-                            res.render('hub',{'name':rows[0].name,'description':rows[0].description});
-                            //res.render('hub',{'name':rows[0].name.replace(/ /g,'_'),'description':rows[0].description});
+                            res.render('hub', {'name': rows[0].name, 'description': rows[0].description});
+                        //res.render('hub',{'name':rows[0].name.replace(/ /g,'_'),'description':rows[0].description});
                         connection.release();
                     });
             });
             break;
         case "genomes.txt":
-            pool.getConnection(function(err, connection) {
+            pool.getConnection(function (err, connection) {
                 connection.query(
-                    'SELECT db from genome where id in (select distinct genome_id FROM labdata where egroup_id = (select id from egroup where shadow=?))',[req.params['shadow']],
+                    'SELECT db from genome where id in (select distinct genome_id FROM labdata where egroup_id = (select id from egroup where shadow=?))', [req.params['shadow']],
                     function (err, rows, fields) {
                         if (!err)
-                            res.render('genomes',{"data": rows});
+                            res.render('genomes', {"data": rows});
                         connection.release();
                     });
             });
@@ -49,21 +49,25 @@ router.get('/:shadow/:file', function(req, res, next) {
             next();
             break;
     }
-}).get('/:shadow/:db/:file', function(req, res, next) {
+}).get('/:shadow/:db/:file', function (req, res, next) {
     res.set('Content-Type', 'text/plain');
 
     console.log(req.params);
-    switch(req.params['file']){
+    switch (req.params['file']) {
         case "trackDb.txt":
-            pool.getConnection(function(err, connection) {
+            pool.getConnection(function (err, connection) {
                 connection.query(
-                    'SELECT id,uid,name4browser from labdata where egroup_id = (select id from egroup where shadow=?) and genome_id in ' +
-                    '((select id from ems.genome where db = ?)) and deleted = 0',[req.params['shadow'],req.params['db']],
+                    'SELECT l.id as id,uid,name4browser, etype ' +
+                    ' from labdata l ' +
+                    ' left join  experimenttype e on e.id=l.experimenttype_id ' +
+                    ' where egroup_id = (select id from egroup where shadow=?)' +
+                    ' and genome_id in ((select id from ems.genome where db = ?)) and deleted = 0'
+                    , [req.params['shadow'], req.params['db']],
                     function (err, rows, fields) {
-                        console.log(err,rows,req.params);
+                        console.log(err, rows, req.params);
                         connection.release();
                         if (err == null && rows.length > 0) {
-                            res.render('trackdb', {"data": rows});
+                            res.render('trackdbs', {"data": rows});
                         } else {
                             next();
                         }
